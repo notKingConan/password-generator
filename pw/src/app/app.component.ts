dc0b3e9d6c8858249979cab91dd64a9312058c45
Dan Osborn <conanosborn@outlook.com>
2023-10-21 20:47:04 +0100

    completed password generator project

--- pw/src/app/app.component.ts
+++ pw/src/app/app.component.ts
@@ -6,10 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  length = 0;
   includeLetters = false;
   includeNumbers = false;
   includeSymbols = false;
   password = '';
+
+  onChangeLength(value: string): void {
+    const parsedValue = +value;
+    if (Number.isNaN(parsedValue)) {
+      console.log('not a number');
+    } else if (!Number.isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
 
   onChangeUseLetters() {
     this.includeLetters = !this.includeLetters;
@@ -24,9 +34,33 @@ export class AppComponent {
   }
 
   onButtonClick() {
-    this.password = 'a test password';
-    console.log('Letters: ' + this.includeLetters);
-    console.log('Numbers: ' + this.includeNumbers);
-    console.log('Symbols: ' + this.includeSymbols);
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let ValidChars = '';
+
+    if (this.includeLetters) ValidChars += letters;
+    if (this.includeNumbers) ValidChars += numbers;
+    if (this.includeSymbols) ValidChars += symbols;
+
+    let generatedPassword = '';
+
+    // if (this.length > 0) {
+    //   if (this.includeLetters || this.includeNumbers || this.includeSymbols) {
+    //     for (let i = 0; i < this.length; i++) {
+    //       const index = Math.floor(Math.random() * ValidChars.length);
+    //       generatedPassword += ValidChars[index];
+    //     }
+    //     this.password = generatedPassword;
+    //   }
+    // } else {
+    //   this.password = '';
+    // }
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * ValidChars.length);
+      generatedPassword += ValidChars[index];
+    }
+    this.password = generatedPassword;
   }
 }
